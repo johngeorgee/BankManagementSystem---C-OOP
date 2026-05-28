56e6577aa5cd19b0d617c1d31bc382c95ed5fa5d
johngeorgee <johngeorgeea@gmail.com>
2026-05-28 16:31:15 +0300

    initial commit

---
+++ BankManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
